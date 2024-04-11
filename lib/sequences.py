e083177ebe33c4ac4fc5c003ef3aa34398268f27
reaganmunene <reagannmunene@gmail.com>
2024-04-11 12:58:28 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    fib_sequence = [0, 1]
+    while len(fib_sequence) < length:
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2]) 
+    print(fib_sequence)
     pass
+
+print_fibonacci(12)
